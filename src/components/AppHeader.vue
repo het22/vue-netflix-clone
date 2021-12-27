7ba4c9d74c8e8141c80d556ced10bfce59936999
het <gocks0922@gmail.com>
2021-12-27 17:28:00 +0900

    markup header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,14 @@
 <template>
-  <header></header>
+  <header>
+    <img class="logo" src="@/assets/logo.png" alt="logo" />
+    <ul class="menus">
+      <li class="menu menu--active">홈</li>
+      <li class="menu">시리즈</li>
+      <li class="menu">영화</li>
+      <li class="menu">NEW! 요즘 대세 콘텐츠</li>
+      <li class="menu">내가 찜한 콘텐츠</li>
+    </ul>
+  </header>
 </template>
 
 <script>
@@ -14,7 +23,31 @@ header {
   top: 0;
   display: flex;
   align-items: center;
+  padding: 0 30px;
   height: 60px;
-  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.logo {
+  height: 36%;
+  cursor: pointer;
+}
+
+.menus {
+  display: flex;
+}
+
+.menu {
+  list-style: none;
+  font-size: 12px;
+  color: #ccc;
+  cursor: pointer;
+}
+.menu--active {
+  font-weight: bold;
+  color: white;
+  pointer-events: none;
+}
+.menu + .menu {
+  margin-left: 18px;
 }
 </style>
