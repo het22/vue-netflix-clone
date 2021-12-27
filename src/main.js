cd78808f799b63b24974d63cf6edf00f83391f7e
het <gocks0922@gmail.com>
2021-12-27 16:44:22 +0900

    add comments for main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  data: { title: 'vue-netflix-clone' },
+
+  // vue instance lifecycle
+  // https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+
+  // h == hyperscript, Virtual DOM에 html을 생성하는 스크립트
+  // render: createElement => createElement(App)
+  render: h => h(App)
+
+  // template을 안쓰고 render를 사용하는 이유?
+  // components: { App },
+  // template: `<app></app>`
+
+  // with 'el' option, no need to call $mount
+  // with '$mount' call, can control mount moment
+  // el: '#app'
+}).$mount('#app');
