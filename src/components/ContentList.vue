a9ee428e7a3a92065772add8b1ca8deaf4dc5855
het <gocks0922@gmail.com>
2021-12-28 18:01:22 +0900

    update contents to show images

--- src/components/ContentList.vue
+++ src/components/ContentList.vue
@@ -2,7 +2,10 @@
   <section>
     <h2>{{ title }}</h2>
     <ul>
-      <li v-for="content in contents" :key="content.id">{{ content.title }}</li>
+      <li v-for="content in contents" :key="content.id">
+        <h3>{{ content.title }}</h3>
+        <img :src="content.thumbnail" :alt="content.title" />
+      </li>
     </ul>
   </section>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,9 +3,21 @@
     <content-list
       title="지금 뜨는 콘텐츠"
       :contents="[
-        { id: 1, title: '콘텐츠1' },
-        { id: 2, title: '콘텐츠3' },
-        { id: 3, title: '콘텐츠3' }
+        {
+          id: 1,
+          title: '콘텐츠1',
+          thumbnail: 'https://picsum.photos/300/200?random=1'
+        },
+        {
+          id: 2,
+          title: '콘텐츠2',
+          thumbnail: 'https://picsum.photos/300/200?random=2'
+        },
+        {
+          id: 3,
+          title: '콘텐츠3',
+          thumbnail: 'https://picsum.photos/300/200?random=3'
+        }
       ]"
     />
   </div>
