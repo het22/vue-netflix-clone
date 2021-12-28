ee210a7f87deb71494b4b0a3649c59e651fb5f5e
het <gocks0922@gmail.com>
2021-12-28 17:57:15 +0900

    add content-list

---
+++ src/components/ContentList.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h2>{{ title }}</h2>
+    <ul>
+      <li v-for="content in contents" :key="content.id">{{ content.title }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'content-list',
+  props: ['title', 'contents']
+};
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>home</div>
+  <div class="home">
+    <content-list
+      title="지금 뜨는 콘텐츠"
+      :contents="[
+        { id: 1, title: '콘텐츠1' },
+        { id: 2, title: '콘텐츠3' },
+        { id: 3, title: '콘텐츠3' }
+      ]"
+    />
+  </div>
 </template>
 
 <script>
+import ContentList from '../components/ContentList.vue';
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: { ContentList }
 };
 </script>
