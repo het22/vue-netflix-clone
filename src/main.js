5c36740a3695717a77bbd775206bc6b6cb747e71
het <gocks0922@gmail.com>
2021-12-31 13:38:50 +0900

    remove comments

--- src/main.js
+++ src/main.js
@@ -7,29 +7,6 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 new Vue({
-  data: { title: 'vue-netflix-clone' },
   router,
-
-  // vue instance lifecycle
-  // https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
-  beforeCreate() {},
-  created() {},
-  beforeMount() {},
-  mounted() {},
-  beforeUpdate() {},
-  updated() {},
-  beforeDestroy() {},
-  destroyed() {},
-
-  // h == hyperscript, Virtual DOM에 html을 생성하는 스크립트
-  // render: createElement => createElement(App)
   render: h => h(App)
-
-  // template을 안쓰고 render를 사용하는 이유?
-  // components: { App },
-  // template: `<app></app>`
-
-  // with 'el' option, no need to call $mount
-  // with '$mount' call, can control mount moment
-  // el: '#app'
 }).$mount('#app');
